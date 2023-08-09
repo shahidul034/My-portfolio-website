6811c08bc299c17161028dbd9706b098a3c6c102
Shahidul Shakib <31099713+shahidul034@users.noreply.github.com>
2023-08-09 12:04:58 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
